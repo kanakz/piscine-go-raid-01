aa97ca6a7e9d90c17b587165636910a66e896e2a
miron-developer <miron.arystan@mail.ru>
2019-10-19 16:58:52 +0600

    r1e1

--- raid1a.go
+++ raid1a.go
@@ -5,17 +5,17 @@ import "github.com/01-edu/z01"
 func Raid1a(x, y int) {
 	if x == 1 && y == 1 {
 		z01.PrintRune('o')
-		z01.PrintRune('\n')
+		z01.PrintRune(10)
 	} else if x == 2 || y == 2 {
 		if x == 2 && y == 1 {
 			z01.PrintRune('o')
 			z01.PrintRune('o')
-			z01.PrintRune('\n')
+			z01.PrintRune(10)
 		} else if y == 2 && x == 1 {
 			z01.PrintRune('o')
-			z01.PrintRune('\n')
+			z01.PrintRune(10)
 			z01.PrintRune('o')
-			z01.PrintRune('\n')
+			z01.PrintRune(10)
 		} else if (x == 2 && y > 2) || (y == 2 && x > 2) {
 			for i := 1; i <= y; i++ {
 				for j := 1; j <= x; j++ {
@@ -34,10 +34,10 @@ func Raid1a(x, y int) {
 		} else {
 			z01.PrintRune('o')
 			z01.PrintRune('o')
-			z01.PrintRune('\n')
+			z01.PrintRune(10)
 			z01.PrintRune('o')
 			z01.PrintRune('o')
-			z01.PrintRune('\n')
+			z01.PrintRune(10)
 		}
 	} else if x >= 2 || y >= 2 {
 		for i := 1; i <= y; i++ {
@@ -52,7 +52,7 @@ func Raid1a(x, y int) {
 					z01.PrintRune(' ')
 				}
 			}
-			z01.PrintRune('\n')
+			z01.PrintRune(10)
 		}
 	} else {
 		for i := 1; i <= y; i++ {
@@ -63,27 +63,27 @@ func Raid1a(x, y int) {
 
 func main() {
 	Raid1a(1, 1)
-	z01.PrintRune('\n')
+	z01.PrintRune(10)
 	Raid1a(2, 1)
-	z01.PrintRune('\n')
+	z01.PrintRune(10)
 	Raid1a(1, 2)
-	z01.PrintRune('\n')
+	z01.PrintRune(10)
 	Raid1a(2, 2)
-	z01.PrintRune('\n')
+	z01.PrintRune(10)
 	Raid1a(3, 3)
-	z01.PrintRune('\n')
+	z01.PrintRune(10)
 	Raid1a(-1, 3)
-	z01.PrintRune('\n')
+	z01.PrintRune(10)
 	Raid1a(1, 4)
-	z01.PrintRune('\n')
+	z01.PrintRune(10)
 	Raid1a(4, 1)
-	z01.PrintRune('\n')
+	z01.PrintRune(10)
 	Raid1a(5, 3)
-	z01.PrintRune('\n')
+	z01.PrintRune(10)
 	Raid1a(2, 19)
-	z01.PrintRune('\n')
+	z01.PrintRune(10)
 	Raid1a(20, 2)
-	z01.PrintRune('\n')
+	z01.PrintRune(10)
 	Raid1a(-1, 3)
-	z01.PrintRune('\n')
+	z01.PrintRune(10)
 }
