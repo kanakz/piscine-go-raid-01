5a6ba8decd16494ec1131258abbcc4f0961e78d9
miron-developer <miron.arystan@mail.ru>
2019-10-19 16:57:21 +0600

    r1e1

--- raid1a.go
+++ raid1a.go
@@ -7,15 +7,30 @@ func Raid1a(x, y int) {
 		z01.PrintRune('o')
 		z01.PrintRune('\n')
 	} else if x == 2 || y == 2 {
-		if x == 2 && y != 2 {
+		if x == 2 && y == 1 {
 			z01.PrintRune('o')
 			z01.PrintRune('o')
 			z01.PrintRune('\n')
-		} else if y == 2 && x != 2 {
+		} else if y == 2 && x == 1 {
 			z01.PrintRune('o')
 			z01.PrintRune('\n')
 			z01.PrintRune('o')
 			z01.PrintRune('\n')
+		} else if (x == 2 && y > 2) || (y == 2 && x > 2) {
+			for i := 1; i <= y; i++ {
+				for j := 1; j <= x; j++ {
+					if (i == 1 && j == 1) || (i == y && j == 1) || (j == x && i == 1) || (i == y && j == x) {
+						z01.PrintRune('o')
+					} else if j == 1 || j == x {
+						z01.PrintRune('|')
+					} else if i == 1 || i == y {
+						z01.PrintRune('-')
+					} else {
+						z01.PrintRune(' ')
+					}
+				}
+				z01.PrintRune(10)
+			}
 		} else {
 			z01.PrintRune('o')
 			z01.PrintRune('o')
@@ -24,7 +39,7 @@ func Raid1a(x, y int) {
 			z01.PrintRune('o')
 			z01.PrintRune('\n')
 		}
-	} else if x > 2 || y > 2 {
+	} else if x >= 2 || y >= 2 {
 		for i := 1; i <= y; i++ {
 			for j := 1; j <= x; j++ {
 				if (i == 1 && j == 1) || (i == y && j == 1) || (j == x && i == 1) || (i == y && j == x) {
@@ -41,7 +56,7 @@ func Raid1a(x, y int) {
 		}
 	} else {
 		for i := 1; i <= y; i++ {
-			z01.PrintRune('\n')
+			z01.PrintRune(10)
 		}
 	}
 }
@@ -57,11 +72,17 @@ func main() {
 	z01.PrintRune('\n')
 	Raid1a(3, 3)
 	z01.PrintRune('\n')
+	Raid1a(-1, 3)
+	z01.PrintRune('\n')
 	Raid1a(1, 4)
 	z01.PrintRune('\n')
 	Raid1a(4, 1)
 	z01.PrintRune('\n')
 	Raid1a(5, 3)
+	z01.PrintRune('\n')
+	Raid1a(2, 19)
+	z01.PrintRune('\n')
+	Raid1a(20, 2)
 	z01.PrintRune('\n')
 	Raid1a(-1, 3)
 	z01.PrintRune('\n')
