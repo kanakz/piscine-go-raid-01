2cdac5b1291d3049917073e969308763216d516a
miron-developer <miron.arystan@mail.ru>
2019-10-19 19:00:58 +0600

    fix1

--- raid1a.go
+++ raid1a.go
@@ -5,43 +5,7 @@ import "github.com/01-edu/z01"
 func Raid1a(x, y int) {
 	if x < 0 || y < 0 {
 
-	} else if x == 1 && y == 1 {
-		z01.PrintRune('o')
-		z01.PrintRune('\n')
-	} else if x == 2 || y == 2 {
-		if x == 2 && y == 1 {
-			z01.PrintRune('o')
-			z01.PrintRune('o')
-			z01.PrintRune('\n')
-		} else if y == 2 && x == 1 {
-			z01.PrintRune('o')
-			z01.PrintRune('\n')
-			z01.PrintRune('o')
-			z01.PrintRune('\n')
-		} else if (x == 2 && y > 2) || (y == 2 && x > 2) {
-			for i := 1; i <= y; i++ {
-				for j := 1; j <= x; j++ {
-					if (i == 1 && j == 1) || (i == y && j == 1) || (j == x && i == 1) || (i == y && j == x) {
-						z01.PrintRune('o')
-					} else if j == 1 || j == x {
-						z01.PrintRune('|')
-					} else if i == 1 || i == y {
-						z01.PrintRune('-')
-					} else {
-						z01.PrintRune(' ')
-					}
-				}
-				z01.PrintRune(10)
-			}
-		} else if x == 2 && y == 2 {
-			z01.PrintRune('o')
-			z01.PrintRune('o')
-			z01.PrintRune('\n')
-			z01.PrintRune('o')
-			z01.PrintRune('o')
-			z01.PrintRune('\n')
-		}
-	} else if x >= 2 || y >= 2 {
+	} else if x >= 1 || y >= 1 {
 		for i := 1; i <= y; i++ {
 			for j := 1; j <= x; j++ {
 				if (i == 1 && j == 1) || (i == y && j == 1) || (j == x && i == 1) || (i == y && j == x) {
